a808aadbb102f927ff61e2a9ad16fe3067a9a9ed
Ernesto G <96400222+gernesto14@users.noreply.github.com>
2023-06-14 18:34:15 -0400

    Update index.js file path for sound file

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ for (let i = 0; i < btn.length; i++) {
   $(btn[i]).mouseover((e) => {
     btn[i].style.color = "green";
 
-    const tom1 = new Audio("sounds/tom-1.mp3");
-    const tom2 = new Audio("sounds/tom-2.mp3");
-    const tom3 = new Audio("sounds/tom-3.mp3");
-    const tom4 = new Audio("sounds/tom-4.mp3");
-    const snare = new Audio("sounds/snare.mp3");
-    const crash = new Audio("sounds/crash.mp3");
-    const kick = new Audio("sounds/kick-bass.mp3");
+    const tom1 = new Audio("/sounds/tom-1.mp3");
+    const tom2 = new Audio("/sounds/tom-2.mp3");
+    const tom3 = new Audio("/sounds/tom-3.mp3");
+    const tom4 = new Audio("/sounds/tom-4.mp3");
+    const snare = new Audio("/sounds/snare.mp3");
+    const crash = new Audio("/sounds/crash.mp3");
+    const kick = new Audio("/sounds/kick-bass.mp3");
 
     switch (i) {
       case 0:
